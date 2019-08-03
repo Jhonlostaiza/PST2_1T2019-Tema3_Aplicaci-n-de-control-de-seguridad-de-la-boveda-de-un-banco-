6cf68cc066adceca80b01e394fb41cfee4a065f5
ProDexter_1997 <carlo68rami@gmail.com>
2019-08-03 13:44:32 -0500

    Creacion del Activity principal y el activity de bobeda

---
+++ app/src/main/java/com/example/sistemadesiguridad/activity_bobeda.java
@@ -0,0 +1,14 @@
+package com.example.sistemadesiguridad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_bobeda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bobeda);
+    }
+}
