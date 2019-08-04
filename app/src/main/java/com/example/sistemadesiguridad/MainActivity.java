dee5f2dc0714872d4fc1319be49a49b5b4422907
dlguerre <dlguerre@espol.edu.ec>
2019-08-04 03:11:33 -0500

    Creacion de ventana editar y distintos metodos para funciones

--- app/src/main/java/com/example/sistemadesiguridad/MainActivity.java
+++ app/src/main/java/com/example/sistemadesiguridad/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.sistemadesiguridad;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void Registrarse(View view) {
+        Intent i = new Intent(this, activity_registro.class );
+        startActivity(i);
+    }
+
+    public void Ingresar(View view) {
+        Intent i = new Intent(this, activity_bobeda.class );
+        startActivity(i);
+    }
+
+
 }
+
