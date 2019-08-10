d4270420996f8856f4d4dc8facce1161daac3518
elibemar <1234elizamar>
2019-08-10 13:15:16 -0500

    comiit

---
+++ app/src/main/java/com/example/sistemadesiguridad/activity_editar.java
@@ -0,0 +1,22 @@
+package com.example.sistemadesiguridad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class activity_editar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar);
+    }
+
+    public void Regresar(View view) {
+        Intent i = new Intent(this, MainActivity.class );
+        startActivity(i);
+    }
+
+}
