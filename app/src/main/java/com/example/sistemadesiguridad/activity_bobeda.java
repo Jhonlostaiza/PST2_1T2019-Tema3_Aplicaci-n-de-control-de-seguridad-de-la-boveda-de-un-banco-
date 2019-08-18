369485f5b96c4fde76ac32eb9bb8226ed177a11c
Jhonlostaiza <jhoelope@espol.edu.ec>
2019-08-18 12:39:43 -0500

    Clase para realizar la apertura de la puerta
    esta clase permite la apertura y cierre de la puerta

--- app/src/main/java/com/example/sistemadesiguridad/activity_bobeda.java
+++ app/src/main/java/com/example/sistemadesiguridad/activity_bobeda.java
@@ -1,12 +1,20 @@
 package com.example.sistemadesiguridad;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 public class activity_bobeda extends AppCompatActivity {
@@ -38,4 +46,58 @@ public class activity_bobeda extends AppCompatActivity {
         Intent i = new Intent(this, MainActivity.class );
         startActivity(i);
     }
+    public void abrirpuerta(View view){
+        final boolean abrir=true;//ingresa la condicion de apertura de la puerta
+        if(abrir==true){
+            Response.Listener<String> respuesta = new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONObject jsonRespuesta = new JSONObject(response);
+                        boolean ok = jsonRespuesta.getBoolean("sucess");//si se logro el cambio dentro de la base de datos se procede 
+                        if (ok == true){
+                                //en esta parte se pone que se realiza en el app cuando ya se abre la puerta
+                            //cambiar de color el indicador cosas asi io que se XD
+                        } else {
+                            AlertDialog.Builder alerta = new AlertDialog.Builder(activity_bobeda.this);//si el respuesta sucess de la base es false no existe el usuario en la base de datos y se envia el mensaje de alerta
+                            alerta.setMessage("Error en apertura").setNegativeButton("Reintentar", null).create().show();
+                        }
+                    } catch (JSONException e) {
+                        e.getMessage();
+                    }
+                }
+            };
+            puerta r = new puerta(abrir, respuesta);
+            RequestQueue cola = Volley.newRequestQueue(activity_bobeda.this);
+            cola.add(r);
+        }
+    }
+    public void cierrepuerta(View view){
+        final boolean abrir=false;
+        if(abrir==true){
+            Response.Listener<String> respuesta = new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONObject jsonRespuesta = new JSONObject(response);
+                        boolean ok = jsonRespuesta.getBoolean("sucess");
+                        if (ok == true){
+                            //en esta parte se pone que se realiza en el app cuando ya se abre la puerta
+                            //cambiar de color el indicador cosas asi io que se XD
+                        } else {
+                            AlertDialog.Builder alerta = new AlertDialog.Builder(activity_bobeda.this);//si el respuesta sucess de la base es false no existe el usuario en la base de datos y se envia el mensaje de alerta
+                            alerta.setMessage("Error en apertura").setNegativeButton("Reintentar", null).create().show();
+                        }
+                    } catch (JSONException e) {
+                        e.getMessage();
+                    }
+                }
+            };
+            puerta r = new puerta(abrir, respuesta);
+            RequestQueue cola = Volley.newRequestQueue(activity_bobeda.this);
+            cola.add(r);
+        }
+    }
 }
+
+
---
+++ app/src/main/java/com/example/sistemadesiguridad/puerta.java
@@ -0,0 +1,23 @@
+package com.example.sistemadesiguridad;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class puerta extends StringRequest {
+    private static final String ruta="https://odinpst.000webhostapp.com/puerta.php";
+    private Map<String, String> parametros;
+
+    public puerta (boolean estado, Response.Listener<String> listener) {
+        super(Request.Method.POST, ruta,listener,null);
+        parametros = new HashMap<>();
+        parametros.put("estado", estado+"");
+    }
+    @Override
+    protected Map<String, String> getParams(){
+        return parametros;
+    }
+}
